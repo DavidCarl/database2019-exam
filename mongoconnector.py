0dc35ec93dea11f423cef12f1e87ef206172c6df
David <david.martin.carl@gmail.com>
2019-05-25 16:42:35 +0200

    More finetune, mongoDB prep

--- mongoconnector.py
+++ mongoconnector.py
@@ -2,8 +2,11 @@ import pymongo
 from pymongo import MongoClient
 from bson.son import SON
 from pprint import pprint
+import filemanager
 
 
-client = MongoClient()
-db = client.tweet
-training = db.training
+mongodb_config = filemanager.load_file('configs/mysql.json')
+
+client = MongoClient(mongodb_config['ip'], mongodb_config['port'])
+# db = client.tweet
+# training = db.training
